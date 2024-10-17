d2747e80a5e9457eb6d6d1e6cc984f7676e36bd7
M-RameshKumar <rameshmsamy333@gmail.com>
2024-10-17 19:22:12 +0530

    final commit

--- JDBCConsoleProject/src/com/techpalle/JDBCproject.java
+++ JDBCConsoleProject/src/com/techpalle/JDBCproject.java
@@ -3,8 +3,12 @@ package com.techpalle;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 
 /*
  *  Project Name: JDBC Console Project
@@ -15,14 +19,212 @@ import java.sql.Statement;
 
 
 public class JDBCproject {
+	
+	public static int count=1; //static variable accessed by only static method
 
 	public static void main(String[] args) {
-
-		// call creating function - so that it creates table
+		
+		/*
+		 * let ask user input...
+		 */
+		
+		Scanner s= new Scanner(System.in);
+		
 		Dao d=new Dao();
-//		d.creating();
-//		d.inserting(1,"ram", 25000); // this will insert first employee
-		d.inserting(2, "ramesh", 0);  // this will insert second employee
+		
+		int option;
+		int pos;
+		
+		
+		try {
+			
+		
+		System.out.println("WELCOME TO JDBC CONSOLE PROJECT");
+		do {
+			
+			System.out.println("------------------CHOOSE CORRECT OPTION------------------------------");
+			System.out.println("1: creating table");
+			System.out.println("2: inserting table");
+			System.out.println("3: update the row of table");
+			System.out.println("4: delete a row of table");
+			System.out.println("5: read all rows");
+			System.out.println("6: read one rows");
+			System.out.println("0: exit");
+			
+			option=s.nextInt();
+			
+			
+			
+			switch(option) {
+	
+			
+			
+			
+			
+			
+			case 6:
+									try {
+										System.out.println("Enter the Eid for disply:");
+										pos=s.nextInt();
+										
+										if(pos>0) {
+											d.display(pos);
+										}
+										else {
+											System.out.println("invalid index");
+										}
+										
+									}
+									catch(InputMismatchException e) {
+										System.out.println("INVALID INPUT");
+									}
+								
+								break;
+			
+			
+			case 5:
+		
+				d.displayAll();
+				break;
+			
+			
+			
+			case 4:
+				
+								try {
+									System.out.println("enter eid for");
+									pos=s.nextInt();
+									
+									if(pos>=0) {
+									
+									d.deleting(pos);
+									}
+									else {
+										System.out.println("Invalid Index");
+									}
+									
+									
+								}
+								catch(InputMismatchException e) {
+									System.out.println("INVALID INPUT");
+								}
+								
+								break;
+			
+			
+			
+			
+			case 3:
+				
+								try {	
+									//we have read eno and new salary from keyboard
+									
+									System.out.println("enter eno and new salary...");
+									int no=s.nextInt();
+									s.nextLine();
+									
+									int newsal=s.nextInt();
+									
+									d.updating(no, newsal);
+								}
+								
+								
+								catch(InputMismatchException e) {
+									System.out.println("Invalid Input");
+									s.nextLine();
+								}
+									catch(Exception e) {
+										System.out.println("Something went wrong ..try..again");
+										
+									}
+								break;
+			
+			case 2:
+							System.out.println("ENTER EN0 AS INT ,ENAME AS STRING ,ESAL AS INT SEQUENTIALLY...");
+									try {
+										int eno=s.nextInt(); //user enters a number into eno
+										//let us remove extra enter key 1.number 2.name use this
+										
+										s.nextLine();
+										
+										String ename=s.next();//user enters string into ename
+										
+										int esal=s.nextInt(); //user enters a number into esal
+										
+										System.out.println("OK INSERTING...");
+					
+									d.inserting(eno, ename, esal);
+						     	   }
+									
+									catch(InputMismatchException e)
+									{
+										System.out.println(" INVALID INPUT");
+										s.nextLine(); // Clear the scanner buffer in case of invalid input
+									}
+									catch(Exception e) {
+										System.out.println(e.getMessage());
+									}
+								
+							break;
+			
+	   
+			case 1:
+								try {
+						
+				                if(count==1) {	
+				                
+				                	System.out.println("OK I AM CREATING...");
+				                	d.creating();
+				                	count++;
+				                	
+				                }
+				                else {
+				                	System.out.println("we canot create same table twice");
+				                }
+				                
+								
+								} catch(InputMismatchException e) { 
+									
+									System.out.println("Please Enter applicable data");
+								}
+								break;
+							
+				
+			
+			
+			case 0:
+								System.out.println("OK I AM EXITING... ");
+								
+								try {
+									Thread.sleep(3000);
+									
+									
+								} catch (InterruptedException e) {
+									
+									
+				                     System.out.println("SOME ISSUE WHILE EXITING");
+									 e.printStackTrace();
+								}
+								break;
+								
+								default:
+									System.out.println("Choose valid option");
+					              
+							
+						
+						
+						}
+						
+						
+					}
+					while(option!=0);
+					}
+						catch(InputMismatchException e) {
+							
+							s.nextLine();
+						}
+
+
 		
 		
 	}
@@ -43,9 +245,22 @@ class  Dao{
 	
 	
 	
+	//below we create for avoiding code duplication
+	
+	
+		
+		String Driver="com.mysql.cj.jdbc.Driver";
+		String url="jdbc:mysql://localhost:3306/palle";
+		String user="root";
+		String pass="ramesh123";
+		
+		
+
 	
 	
     //	in this method we wil create employee table
+	
+	
 	
 	public void creating(){		
 		
@@ -55,14 +270,14 @@ class  Dao{
 		
 			
 			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
+				Class.forName(Driver);
 				
-				 c=DriverManager.getConnection("jdbc:mysql://localhost:3306/palle","root", "ramesh123");
+				 c=DriverManager.getConnection(url,user, pass);
 				
 				 s=c.createStatement();
 				
 				s.executeUpdate("CREATE TABLE EMPLOYEE (ID INT PRIMARY KEY AUTO_INCREMENT , ENAME VARCHAR(50),ESALARY BIGINT) "); 
-				System.out.println("sucessfully table was created");
+				System.out.println("sucessfully table was created...");
 				
 				} 
 			catch (ClassNotFoundException e) { 
@@ -72,7 +287,7 @@ class  Dao{
 				e.printStackTrace();
 			} catch (SQLException e) {
 				System.out.println("Something went wrong with Database");
-				e.printStackTrace();
+				System.out.println(e.getMessage());
 			}
 			finally {
 				
@@ -111,9 +326,9 @@ class  Dao{
 		Connection c=null;
 		
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName(Driver);
 			
-			c=DriverManager.getConnection("jdbc:mysql://localhost:3306/palle", "root", "ramesh123");
+			c=DriverManager.getConnection(url,user,pass);
 			
               String query="insert into employee values(?,?,?)"; //for prepeared statement we need prepare first
               
@@ -126,7 +341,10 @@ class  Dao{
               ps.setInt(3, esalary); // 3rd ?
               
               ps.executeUpdate();
-              System.out.println("sucessfully added!!");
+              
+            
+              
+              System.out.println("Sucessfully Inserted...");
               
 			
 			
@@ -140,6 +358,7 @@ class  Dao{
 		
 		catch (SQLException e) {
 			System.out.println("someting went wrong with database");
+			System.out.println(e.getMessage());
 			
 		}
 		 catch(Exception e) {
@@ -167,15 +386,281 @@ class  Dao{
 	}
 	
 	
+	//in below method we will update ...for a given employee no update new salary
+	
+	
+	public void updating(int eno,int newsal) {
+		
+		Connection c=null;
+		PreparedStatement ps= null;
+		
+		              try {
+						Class.forName(Driver);
+						c=DriverManager.getConnection(url,user,pass);
+						
+						
+						String query="update employee set ESALARY=? where ID=?"; 
+						
+						ps=c.prepareStatement(query);
+						
+						ps.setInt(1, newsal);
+						ps.setInt(2, eno);
+						
+						
+						int rowAffected=ps.executeUpdate(); //executeUpdate return affected rows 
+						//It returns an integer value indicating the number of rows that were affected by the query.
+						
+						if(rowAffected >0) {
+							System.out.println("Successfully Updated...");
+						
+						}
+						else {
+							System.out.println("No employee found with id:"+eno);
+						}
+						
+						
+					
+						
+					} catch (ClassNotFoundException e) {
+						
+						System.out.println("Driver is not loaded");
+  					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+  						System.out.println(" something wrong with database");
+					}
+		              finally {
+		            	  try {
+		            		if(ps!=null) {  
+		            			ps.close();
+		            		}
+		            		if(c!=null) {
+		            			c.close();
+		            		}
+								
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+		            	  
+		              }
+		
+	}
+	
+
+
+//below method we can delete a particular record 
+	public void deleting(int eid) {
+		
+		PreparedStatement ps=null;
+		Connection c=null;
+		
+		try {
+			Class.forName(Driver);
+			
+			c=DriverManager.getConnection(url, user, pass);
+			
+			
+			String query="DELETE FROM EMPLOYEE WHERE ID=?";
+			
+			ps=c.prepareStatement(query);
+			
+			ps.setInt(1, eid);
+			
+			 int count=ps.executeUpdate();
+			if(count>0) {
+				System.out.println("SUCESSFULLY DELETED RECORD...");
+			}
+			else {
+				System.out.println("Index out of limit: "+eid);
+			}
+			
+			
+		} catch (ClassNotFoundException e) {
+			
+			System.out.println("DRIVER NOT LOADED");
+			e.printStackTrace();
+		} catch (SQLException e) {
+
+			System.out.println("SOMETHING ISSUE IN DATABASE");
+			e.printStackTrace();
+		}
+		
+		
+		finally {
+			if(ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			if(c!=null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
+		
+		
+		
+	}
+
+	
+	
+	
+	//below method we will read all employee details and display
+	
+	
+	public void displayAll() {
+		
+		Connection c= null;
+		Statement s=null;
+		
+		
+        try {
+			Class.forName(Driver);
+			c=DriverManager.getConnection(url,user,pass);
+			
+			s=c.createStatement();
+			
+			
+		   ResultSet res=s.executeQuery("select * from employee"); //data stored in 'res'
+		
+		  System.out.println("\n-------------------EMPLOYEE TABLE DATA BELOE---------------------------\n");
+		  System.out.println("ID          ENAME          ESALARY");
+		  System.out.println("--          -----          --------");
+		  
+		
+		   while(res.next()) {
+			   
+			   System.out.println(res.getInt(1) +" "+ res.getString(2)+" "+res.getInt(3)); 
+			 
+		   }
+		   System.out.println("-----------------------------------------------------------------------------");
+		
+		
+    	
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Driver is not Loaded");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Sql connection issues");
+			e.printStackTrace();
+		}
+        
+       finally {
+    	   try {
+    		   if(s!=null) {
+    			   s.close();
+    		   }
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	   
+    	   try {
+    		   if(c!=null) {
+    			   c.close();
+    		   }
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       }
+        
+	}
+	
+	
+	//below method for display particular record
+	
+	public void display(int eid) { 
+		
+		
+		PreparedStatement ps=null;
+		Connection c=null;
+		
+		try {
+			Class.forName(Driver);
+			c=DriverManager.getConnection(url, user, pass);
+			
+			
+			String query="SELECT * FROM EMPLOYEE WHERE ID=?";
+			
+			
+	
+			ps=c.prepareStatement(query);
+			
+			ps.setInt(1, eid);
+		
+		   ResultSet res= ps.executeQuery();
+		   
+		   
+		   
+		   if(res.next()) {
+			   System.out.println(res.getInt(1)+"  "+res.getString(2)+"  "+res.getInt(3));
+			   System.out.println("Sucessfully Displayed...");
+		   }
+		   else {
+			   System.out.println("Index out of limit:"+eid);
+		   }
+		   
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			
+			System.out.println("DRIVER NOT LOADED");
+
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			System.out.println("SOME ISSUE IN DATABASE");
+			e.printStackTrace();
+		}
+		
+		
+		finally {
+			
+			if(ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(c!=null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		
+		
+		
+	}
+	
+
+	
 	
 	
 }
-
-
-
-
-
-
 
 
 
